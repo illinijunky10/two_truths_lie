2bf31eb7cada68a91356d6f8462179d370708113
Emma <emmakepple@gmail.com>
2018-06-27 19:32:41 -0500

    finished project

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 $(document).ready( function (){
 
+$('.title1').show();
 
+$('.title2').hide();
 
+$('.title3').hide();
+
+$('#button1').on('click' , function () {
+  $('.title1').hide();
+  $('.title2').show();
+  $('.title3').hide();
+  $('.title2').addClass('incorrectT1');
+  $(this).addClass('wrong');
+  $('#button2').removeClass('wrong');
+  $('#button3').removeClass('wrong');
+  $('.title3').removeClass('correctT1');
+});
+
+$('#button3').on('click', function () {
+  $('.title1').hide();
+  $('.title2').show();
+  $('.title3').hide();
+  $('.title2').addClass('incorrectT1');
+  $(this).addClass('wrong');
+  $('#button1').removeClass('wrong');
+  $('#button2').removeClass('correct');
+  $('.title3').removeClass('correctT1');
+});
+
+$('#button2').on('click', function () {
+  $('.title1').hide();
+  $('.title2').hide();
+  $('.title3').show();
+  $('.title3').addClass('correctT1');
+  $(this).addClass('correct');
+  $('#button1').removeClass('wrong');
+  $('#button3').removeClass('wrong');
+  $('.title1').removeClass('incorrectT1');
+});
 
 });
